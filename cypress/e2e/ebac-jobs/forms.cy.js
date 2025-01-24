6de2da0a9befac05d9c80221a1504105b673f8b1
maxh33 <contact@maxhaider.dev>
2025-01-24 16:36:16 -0300

    fix: update selectors and add test for applications forms

---
+++ cypress/e2e/ebac-jobs/forms.cy.js
@@ -0,0 +1,28 @@
+/// <reference types="cypress" />
+
+describe('Tests for application page - Forms', () => {
+    beforeEach(() => {
+        cy.visit('https://ebac-jobs-e2e.vercel.app/')
+    })
+    it('Should bring the user to the applications forms', () => {
+        cy.wait(1000)
+        cy.get(' .Vaga_vagaLink__DeFkk', {timeout: 1000}).first().click()
+        cy.get('input').should('have.length', 7)
+    })
+
+    it('Should fill the applications forms', () => {
+        cy.wait(1000)
+        cy.get('.Vaga_vagaLink__DeFkk').first().click()
+        cy.get('input[name="nome-completo"]').type('João da Silva')
+        cy.get('input[name="email"]').type('satoshi@nakamoto.btc')
+        cy.get('input[name="telefone"]').type('11 999999999')
+        cy.get('input[name="endereco"]').type('Rua borba gato, 50')
+        cy.get('#linux').check()
+        cy.get('select[name="escolaridade"').select('outros')
+        cy.get('.Aplicacao_button__tw2AE', {timeout: 3000}).should('be.visible').click()
+
+        cy.on('window:alert', (conteudo) => {
+            expect(conteudo).contains('Obrigado pela candidatura!')
+        })
+    })
+})
--- cypress/e2e/joiasmax-jobs/home.cy.js
+++ cypress/e2e/joiasmax-jobs/home.cy.js
@@ -1,14 +1,15 @@
 /// <reference types="cypress" />
 
 describe('JoiasMax Jobs - Home', () => {
-    it('Should display 7 jobs', () => {
+    beforeEach(() => {
         cy.visit('https://base-css-in-js.vercel.app/')
+    })
+    it('Should display 7 jobs', () => {
         cy.wait(1000)
         cy.get(' .sc-dAlyuH.bMdtrN > li', {timeout: 1000}).should('have.length', 7)
     })
 
     it('Should filter for fullstack', () => {
-        cy.visit('https://base-css-in-js.vercel.app/')
         cy.get('.sc-imWYAI.ivcQil').type('fullstack{enter}')
         cy.get(' button[type="submit"]').click()
         cy.get(' .sc-dAlyuH.bMdtrN > li', {timeout: 1000}).should('have.length', 1)
